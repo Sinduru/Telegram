223098c17c66d626f02b5d47761d69f85f8a920a
Sinduru <69154812+Sinduru@users.noreply.github.com>
2024-11-30 22:31:21 +0100

    Update telegram_bot2.py

--- telegram_bot2.py
+++ telegram_bot2.py
@@ -77,7 +77,7 @@ async def send_material(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "Tack för ditt bidrag, ditt material skickas vidare till gruppen. "
         "En av ägarna eller våra mod(s) kommer undersöka ditt material. "
         "Om du blir godkänd får du tillgång till gruppen.\n"
-        "t.me/2fEGCLjXZFkwYTA0"
+        "https://t.me/+2fEGCLjXZFkwYTA0"
     )
     
     await update.message.reply_text(thank_you_message)
